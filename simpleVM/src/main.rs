d243bbd63a51d24b0cb5e0d09b9fe7904688113f
Matthew Elbing <melbing1@protonmail.ch>
2022-02-05 14:38:04 -0500

    Add some initial structures

--- simpleVM/src/main.rs
+++ simpleVM/src/main.rs
@@ -1,12 +1,30 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-struct Registers {
+use std::collections::HashMap;
+
+#[derive(Default)]
+struct Register {
     ax: u32,
     bx: u32,
     cx: u32,
     dx: u32,
 }
+
+#[derive(Default)]
+struct Address {
+    loc: u32,
+    memmap: HashMap<u32, String>,
+}
+
+// Instructions
+fn read(a: &Address) -> String {
+    if a.loc < a.memmap.capacity() as usize { a.memmap.get(&a.loc) }
+    else { println!("SegFault") }
+}
+
+
 fn main() {
     println!("Hello, world!");
+    let addr = Register::default;
 }
